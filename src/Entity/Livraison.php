37c1701a681e949c829f5d6ebda2abcfa838ae19
TanMess <breant121@gmail.com>
2024-11-14 15:06:50 +0100

    EntityLivraison

---
+++ src/Entity/Livraison.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LivraisonRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LivraisonRepository::class)]
+class Livraison
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $date_livraison = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDateLivraison(): ?\DateTimeInterface
+    {
+        return $this->date_livraison;
+    }
+
+    public function setDateLivraison(\DateTimeInterface $date_livraison): static
+    {
+        $this->date_livraison = $date_livraison;
+
+        return $this;
+    }
+}
